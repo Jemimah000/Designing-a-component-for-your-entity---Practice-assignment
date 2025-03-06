0ffe9395ca56629b15c88eaeadac2d66b6b87067
Jemimah000 <28dora.nobi@gmail.com>
2025-03-06 13:41:26 +0530

    updated

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,21 @@
-// write the component code here
+import React from "react";
+import profilePic from '../assets/image.png';
+function usercard(){
+    const user = {
+        profilePic : profilePic,
+        name : "Manuel Jemimah Mary",
+        email : "jemimahmarymanuel@gmail.com",
+        phone : "8125414142",
+        address : "Anand Gokulam, Teacher's layout, 301, Kommadi Rd, Gandhi Nagar,\nMadhurawada, Visakhapatnam, Andhra Pradesh 530048"
+    };
+    return(
+        <div className="card">
+            <img src={user.profilePic} alt="Profile" className="profile-pic"/>
+            <h2>{user.name}</h2>
+            <p><strong>Email:</strong>{user.email}</p>
+            <p><strong>Phone:</strong>{user.phone}</p>
+            <p className="address"><strong>Address:</strong>{user.address}</p>
+        </div>
+    );
+}
+export default usercard;
